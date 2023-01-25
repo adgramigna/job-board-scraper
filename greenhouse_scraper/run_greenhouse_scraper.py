be5abde2efa24a5fd1fbd34200c6cb8de8b9a998
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-01-24 20:19:16 -0500

    update run script

--- greenhouse_scraper/run_greenhouse_scraper.py
+++ greenhouse_scraper/run_greenhouse_scraper.py
@@ -1,8 +1,9 @@
 import scrapy
+import sys
 from scrapy.crawler import CrawlerProcess
 from greenhouse_scraper.spiders.jobs_outline_spider import JobsOutlineSpider
 from scrapy.utils.project import get_project_settings
 
 process = CrawlerProcess(get_project_settings())
-process.crawl(JobsOutlineSpider)
+process.crawl(JobsOutlineSpider, careers_page_url = sys.argv[1])
 process.start()
