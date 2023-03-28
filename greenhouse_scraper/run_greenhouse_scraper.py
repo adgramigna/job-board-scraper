9085b89de0d62f9ba457f6a7ffc8ee1e87ce60c7
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-03-28 00:01:00 -0400

    add use_existing_html to spider call

--- greenhouse_scraper/run_greenhouse_scraper.py
+++ greenhouse_scraper/run_greenhouse_scraper.py
@@ -1,5 +1,5 @@
-import scrapy
 import sys
+import scrapy
 from scrapy.crawler import CrawlerProcess
 from greenhouse_scraper.spiders.jobs_outline_spider import JobsOutlineSpider
 from greenhouse_scraper.spiders.job_departments_spider import JobDepartmentsSpider
@@ -7,5 +7,5 @@ from scrapy.utils.project import get_project_settings
 
 process = CrawlerProcess(get_project_settings())
 # process.crawl(JobsOutlineSpider, careers_page_url = sys.argv[1])
-process.crawl(JobDepartmentsSpider, careers_page_url = sys.argv[1])
+process.crawl(JobDepartmentsSpider, careers_page_url = sys.argv[1], use_existing_html=False)
 process.start()
