83b03f99c68bf5c7647ef43c06e297b1a7624242
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-03-28 22:48:39 -0400

    correctly parse greenhouse jobs outline

--- greenhouse_scraper/greenhouse_scraper/spiders/jobs_outline_spider.py
+++ greenhouse_scraper/greenhouse_scraper/spiders/jobs_outline_spider.py
@@ -16,13 +16,6 @@ from datetime import datetime
 load_dotenv()
 # logger = logging.getLogger("logger")
 
-
-#TODO: 
-# 1. Add in proper URLs as well as logic to scrape HTML file in s3 or raw website
-    #a. add kwargs to run script as well
-# 2. Create s3 bucket for raw HTML
-# 3. Begin Scraping Greenhouse for 3 different companies
-
 class JobsOutlineSpider(JobDepartmentsSpider):
     name = "jobs_outline"
     allowed_domains = ["boards.greenhouse.io"]
@@ -36,66 +29,26 @@ class JobsOutlineSpider(JobDepartmentsSpider):
     def parse(self, response):
         response_html = self.finalize_response(response)
         selector = Selector(text=response_html, type="html")
-        all_departments = selector.xpath('//section[contains(@class, "level")]')
-        main_departments = selector.xpath('//section[contains(@class, "level-0")]')
-        secondary_departments = selector.xpath('//section[contains(@class, "level-1")]')
         job_openings = selector.xpath('//div[@class="opening"]')
 
-        for i, department in enumerate(main_departments):
-            il = ItemLoader(item=JobsOutlineItem(), selector=department)
-            self.logger.info(f"Parsing row {i+1}, {self.greenhouse_company_name}")
-            # main_department_text = department.xpath(f'//section[contains(@class, "level-0")][{i+1}]/*[self::h1 or self::h2 or self::h3 or self::h4]/text()')
-            dep_xpath = f"//section[contains(@class, 'level-0')][{i+1}]/*[self::h1 or self::h2 or self::h3 or self::h4]/text()"
-            il.replace_xpath(
-                "main_department",
-                dep_xpath
-            )
-            secondary_departments = department.xpath(f'//section[contains(@class, "level-0")][{i+1}]//section[contains(@class, "level-1")]')
-            # self.logger.info(f"{dep_xpath} Department here")
-  
-            job_openings = department.xpath(f'//section[contains(@class, "level-0")][{i+1}]/div[@class="opening"]')
-            for k, opening in enumerate(job_openings):
-                    job_xpath = f'//section[contains(@class, "level-0")][{i+1}]/div[@class="opening"][{k+1}]/a/text()'
-                    dep_id_xpath = f'//section[contains(@class, "level-0")][{i+1}]/div[{k+1}]/@department_id'
-                    il.replace_xpath(
-                        "job_title",
-                        job_xpath
-                    )
-                    il.replace_xpath(
-                        "department_id",
-                        dep_id_xpath
-                    )
-                    # self.logger.info(f'{i} {j} {k} {job_xpath} xpth')
-                    yield il.load_item()
-                    
-            if len(secondary_departments) == 0:
-                il.add_value(
-                    "secondary_department",
-                    None
-                )
-            else:
-                for j, sec_department in enumerate(secondary_departments):
-                    sec_dep_xpath = f"//section[contains(@class, 'level-0')][{i+1}]//section[contains(@class, 'level-1')][{j+1}]/*[self::h1 or self::h2 or self::h3 or self::h4]/text()"
-                    il.replace_xpath(
-                        "secondary_department",
-                        sec_dep_xpath
-                    )
-                # self.logger.info(f"{sec_dep_xpath} sec Department here")
-                    job_openings = sec_department.xpath(f'//section[contains(@class, "level-0")][{i+1}]//section[contains(@class, "level-1")][{j+1}]/div[@class="opening"]')
-                    for k, opening in enumerate(job_openings):
-                        job_xpath = f'//section[contains(@class, "level-0")][{i+1}]/section[contains(@class, "level-1")][{j+1}]/div[@class="opening"][{k+1}]/a/text()'
-                        dep_id_xpath = f'//section[contains(@class, "level-0")][{i+1}]/section[contains(@class, "level-1")][{j+1}]/div[{k+1}]/@department_id'
-                        il.replace_xpath(
-                            "job_title",
-                            job_xpath
-                        )
-                        il.replace_xpath(
-                            "department_id",
-                            dep_id_xpath
-                        )
-                        # self.logger.info(f'{i} {j} {k} {job_xpath} xpth')
-                        yield il.load_item()
-   
+        for i, opening in enumerate(job_openings):
+            il = ItemLoader(item=JobsOutlineItem(), selector=Selector(text=opening.get(),type="html"))
+            self.logger.info(f"Parsing row {i+1}, {self.greenhouse_company_name} {self.name}")
+            nested = il.nested_xpath('//div[@class="opening"]')
+
+            nested.add_xpath("department_ids", "@department_id")
+            nested.add_xpath("office_ids", "@office_id")
+            il.add_xpath("opening_link", "//a/@href")
+            il.add_xpath("opening_title", "//a/text()")
+            il.add_xpath("location", "//span/text()")
+            
+            il.add_value("id", self.determine_row_id(i))
+            il.add_value("created_at", self.created_at)
+            il.add_value("updated_at", self.updated_at)
+            il.add_value("source", self.html_source)
+            # self.logger.info(f'{i} {j} {k} {job_xpath} xpth')
+            yield il.load_item()
+
         # filename = f'{self.greenhouse_company_name}-{self.allowed_domains[0].split(".")[1]}.html'
         # with open(filename, 'wb') as f:
         #     f.write(response.body)
