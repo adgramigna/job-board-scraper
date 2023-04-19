75be18b3579b1fd888f52f1232c1f20bb28d292d
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-04-19 19:24:53 -0400

    comment pipelines

--- greenhouse_scraper/greenhouse_scraper/settings.py
+++ greenhouse_scraper/greenhouse_scraper/settings.py
@@ -73,9 +73,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'greenhouse_scraper.pipelines.GreenhouseScraperPipelineParquet': 300,
-}
+# ITEM_PIPELINES = {
+#    'greenhouse_scraper.pipelines.GreenhouseScraperPipelineParquet': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
