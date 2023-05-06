7e994e79a1e185011a4f26f79fe1d064e77aba42
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-05-05 20:20:47 -0400

    Scrape all sites from database

--- job_board_scraper/run_job_scraper.py
+++ job_board_scraper/run_job_scraper.py
@@ -1,16 +1,27 @@
 import sys
 import scrapy
+import os
+import logging
 from scrapy.crawler import CrawlerProcess
 from job_board_scraper.spiders.greenhouse_jobs_outline_spider import GreenhouseJobsOutlineSpider
 from job_board_scraper.spiders.greenhouse_job_departments_spider import GreenhouseJobDepartmentsSpider
 from job_board_scraper.spiders.lever_jobs_outline_spider import LeverJobsOutlineSpider
+from job_board_scraper.utils.postgres_wrapper import PostgresWrapper
 from scrapy.utils.project import get_project_settings
 
+logger = logging.getLogger("logger")
 process = CrawlerProcess(get_project_settings())
-careers_page_url = sys.argv[1]
-if careers_page_url.split(".")[1] == "greenhouse":
-    process.crawl(GreenhouseJobDepartmentsSpider, careers_page_url = careers_page_url, use_existing_html=True)
-    process.crawl(GreenhouseJobsOutlineSpider, careers_page_url = careers_page_url, use_existing_html=True)
-elif careers_page_url.split(".")[1] == "lever":
-    process.crawl(LeverJobsOutlineSpider, careers_page_url = careers_page_url, use_existing_html=True)
+pg_wrapper = PostgresWrapper()
+cursor = pg_wrapper.cursor()
+cursor.execute(os.environ.get("PAGES_TO_SCRAPE_QUERY"))
+careers_page_urls = cursor.fetchall()
+
+for url in careers_page_urls:
+    careers_page_url = url[0] #UnTuple-ify
+    logger.info(f"url = {careers_page_url} {careers_page_urls}")
+    if careers_page_url.split(".")[1] == "greenhouse":
+        process.crawl(GreenhouseJobDepartmentsSpider, careers_page_url = careers_page_url, use_existing_html=True)
+        process.crawl(GreenhouseJobsOutlineSpider, careers_page_url = careers_page_url, use_existing_html=True)
+    elif careers_page_url.split(".")[1] == "lever":
+        process.crawl(LeverJobsOutlineSpider, careers_page_url = careers_page_url, use_existing_html=True)
 process.start()
