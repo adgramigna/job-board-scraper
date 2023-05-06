dda9595e46d15cc8609844ca03b8ab016e1bac40
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-05-06 01:44:42 -0400

    move closing to the end

--- job_board_scraper/compare_workflow_success.py
+++ job_board_scraper/compare_workflow_success.py
@@ -10,9 +10,6 @@ connection = psycopg2.connect(host=os.environ.get("PG_HOST"), user=os.environ.ge
 cursor = connection.cursor()
 cursor.execute(os.environ.get("COMPARISON_QUERY_EXPECTED"))
 num_expected = cursor.fetchall()[0]
-cursor.close()
-connection.close()
-
 cursor.execute(os.environ.get("COMPARISON_QUERY_ACTUAL"), tuple([time_to_check, time_to_check]))
 num_actual = cursor.fetchall()[0]
 logger.info(f"Num Expected to Scrape: {num_expected}")
@@ -21,4 +18,6 @@ logger.info(f"Num Actually to Scraped: {num_actual}")
 if num_expected != num_actual:
     cursor.execute(os.environ.get("MISMATCHED_URL_QUERY"), tuple([time_to_check, time_to_check]))
     logger.info(f"Mismatched URLs: {cursor.fetchall()}")
+cursor.close()
+connection.close()
 assert num_expected == num_actual
