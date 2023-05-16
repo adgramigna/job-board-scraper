4d69e9033ddcd26acb464f05e92ef14013e63c41
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-05-15 20:01:34 -0400

    add s3 prefix

--- job_board_scraper/job_board_scraper/spiders/greenhouse_job_departments_spider.py
+++ job_board_scraper/job_board_scraper/spiders/greenhouse_job_departments_spider.py
@@ -133,7 +133,7 @@ class GreenhouseJobDepartmentsSpider(scrapy.Spider):
             il.add_value("source", self.html_source)
             il.add_value("company_name", self.company_name)
             il.add_value("run_hash", self.run_hash)
-            il.add_value("raw_html_file_location", self.settings["S3_HTML_BUCKET"]+ "/" + self.s3_html_path)
+            il.add_value("raw_html_file_location", "s3://" + self.settings["S3_HTML_BUCKET"]+ "/" + self.s3_html_path)
             il.add_value("existing_html_used", self.existing_html_used)
 
             yield il.load_item()
