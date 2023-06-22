dec64b89d7eb14397059d3123860cf7f79185c44
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-06-21 21:43:13 -0400

    add expected pages scraped test

---
+++ levergreen_dbt/tests/check_expected_careers_pages_scraped.sql
@@ -0,0 +1,17 @@
+-- here we are checking if the scraper did correctly scrape jobs for each careers_page
+-- If the scraper missed a page due to an unexpected error, we will have mismatches here
+
+with expected_sources as (
+    select distinct url as expected_source
+    from {{ source('levergreen', 'job_board_urls') }}
+    where is_enabled
+),
+
+actual_sources as (
+    select distinct source as actual_source
+    from {{ ref('active_job_postings') }}
+)
+
+select * from expected_sources
+full outer join actual_sources on expected_sources.expected_source = actual_sources.actual_source
+where expected_sources.expected_source is null or actual_sources.actual_source is null
