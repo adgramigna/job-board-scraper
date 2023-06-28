d8b85652d0d803df56bd488d88dbb8f6a13b5522
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-06-27 22:12:15 -0400

    update severity to warn because of rhino no jobs

---
+++ levergreen_dbt/tests/check_expected_careers_pages_scraped_excl_rhino.sql
@@ -0,0 +1,20 @@
+{{ config(severity = 'warn') }}
+
+-- here we are checking if the scraper did correctly scrape jobs for each careers_page
+-- If the scraper missed a page due to an unexpected error, we will have mismatches here
+
+with expected_sources as (
+    select distinct url as expected_source
+    from {{ source('levergreen', 'job_board_urls') }}
+    where is_enabled
+    and url != 'https://boards.greenhouse.io/rhino' --rhino has no job postings active currently
+),
+
+actual_sources as (
+    select distinct source as actual_source
+    from {{ ref('active_job_postings') }}
+)
+
+select * from expected_sources
+full outer join actual_sources on expected_sources.expected_source = actual_sources.actual_source
+where expected_sources.expected_source is null or actual_sources.actual_source is null
