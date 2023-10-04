22a0049ea1dc1c7e806aba553fe61527a807f801
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-10-04 12:07:24 -0400

    parse departments for level 3 down

--- job_board_scraper/job_board_scraper/spiders/greenhouse_job_departments_spider.py
+++ job_board_scraper/job_board_scraper/spiders/greenhouse_job_departments_spider.py
@@ -125,7 +125,7 @@ class GreenhouseJobDepartmentsSpider(scrapy.Spider):
 
         for i, department in enumerate(all_departments):
             il = ItemLoader(item=GreenhouseJobDepartmentsItem(), selector=Selector(text=department.get(),type="html"))
-            dept_loader = il.nested_xpath(f"//section[contains(@class, 'level')]/*[self::h1 or self::h2 or self::h3 or self::h4 or self::h5]")
+            dept_loader = il.nested_xpath(f"//section[contains(@class, 'level')]/*[self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6]")
             self.logger.info(f"Parsing row {i+1}, {self.company_name}, {self.name}")
 
             dept_loader.add_xpath("department_id", "@id")
