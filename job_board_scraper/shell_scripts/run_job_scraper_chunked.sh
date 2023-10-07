146207b8e63082cbcbba828b699dd615131f2e0a
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-10-07 12:19:07 -0400

    add shell script

---
+++ job_board_scraper/shell_scripts/run_job_scraper_chunked.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+cd ..
+total_size=${1}
+chunk_size=${2-20} #default to chunks of 20
+lowest_id=1 #first serial id is 1.
+for ((i=$lowest_id;i<=$total_size;i++))
+do
+    if [ $(($i % $chunk_size)) -eq 0 ]
+    then
+       highest_id=$i
+       python run_job_scraper.py $lowest_id $highest_id 
+       lowest_id=$i
+    fi
+done
+   
