9a09099fefda4974bb8d665f5bbb49483b3fab8a
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-10-08 09:57:41 -0400

    fix conditions

--- levergreen_dbt/models/marts/active_job_postings_prospect.sql
+++ levergreen_dbt/models/marts/active_job_postings_prospect.sql
@@ -1,4 +1,4 @@
 select active_job_postings.* from {{ ref('active_job_postings') }} active_job_postings
-inner join from {{ source('levergreen', 'job_board_urls') }} job_board_urls 
-    on active_job_postings.full_opening_link = job_board_urls.url
+inner join {{ source('levergreen', 'job_board_urls') }} job_board_urls 
+    on active_job_postings.source = job_board_urls.url
 where job_board_urls.is_prospect
