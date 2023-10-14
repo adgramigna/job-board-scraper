c47944b3c5792a3d7df9255ad3246a2154c23b9f
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-10-14 14:58:08 -0400

    Rewrite to include multiprocessing

--- job_board_scraper/run_job_scraper.py
+++ job_board_scraper/run_job_scraper.py
@@ -4,32 +4,48 @@ import os
 import logging
 import psycopg2
 import time
+import multiprocessing
 from scrapy.crawler import CrawlerProcess
 from job_board_scraper.spiders.greenhouse_jobs_outline_spider import GreenhouseJobsOutlineSpider
 from job_board_scraper.spiders.greenhouse_job_departments_spider import GreenhouseJobDepartmentsSpider
 from job_board_scraper.spiders.lever_jobs_outline_spider import LeverJobsOutlineSpider
 from job_board_scraper.utils.postgres_wrapper import PostgresWrapper
 from job_board_scraper.utils import general as util
-from job_board_scraper.utils.scraper_util import parse_args
+from job_board_scraper.utils.scraper_util import get_url_chunks
 from scrapy.utils.project import get_project_settings
 
 logger = logging.getLogger("logger")
-process = CrawlerProcess(get_project_settings())
-connection = psycopg2.connect(host=os.environ.get("PG_HOST"), user=os.environ.get("PG_USER"), password=os.environ.get("PG_PASSWORD"), dbname=os.environ.get("PG_DATABASE"))
-cursor = connection.cursor()
-id_restriction, id_values = parse_args()
-cursor.execute(os.environ.get("PAGES_TO_SCRAPE_QUERY")+id_restriction, id_values)
-careers_page_urls = cursor.fetchall()
-cursor.close()
-connection.close()
 run_hash = util.hash_ids.encode(int(time.time()))
 
-for i, url in enumerate(careers_page_urls):
-    careers_page_url = url[0] #UnTuple-ify
-    logger.info(f"url = {careers_page_url} {careers_page_urls}")
-    if careers_page_url.split(".")[1] == "greenhouse":
-        process.crawl(GreenhouseJobDepartmentsSpider, careers_page_url = careers_page_url, use_existing_html=True, run_hash=run_hash, url_id=i)
-        process.crawl(GreenhouseJobsOutlineSpider, careers_page_url = careers_page_url, use_existing_html=True, run_hash=run_hash, url_id=i)
-    elif careers_page_url.split(".")[1] == "lever":
-        process.crawl(LeverJobsOutlineSpider, careers_page_url = careers_page_url, use_existing_html=True, run_hash=run_hash, url_id=i)
-process.start()
+def run_spider(single_url_chunk, chunk_number):
+    process = CrawlerProcess(get_project_settings())
+    for i, careers_page_url in enumerate(single_url_chunk):
+        logger.info(f"url = {careers_page_url}")
+        if careers_page_url.split(".")[1] == "greenhouse":
+            process.crawl(GreenhouseJobDepartmentsSpider, careers_page_url = careers_page_url, use_existing_html=True, run_hash=run_hash, url_id=chunk_number*i+i)
+            process.crawl(GreenhouseJobsOutlineSpider, careers_page_url = careers_page_url, use_existing_html=True, run_hash=run_hash, url_id=chunk_number*i+i)
+        elif careers_page_url.split(".")[1] == "lever":
+            process.crawl(LeverJobsOutlineSpider, careers_page_url = careers_page_url, use_existing_html=True, run_hash=run_hash, url_id=chunk_number*i+i)
+    process.start()
+
+if __name__ == '__main__':
+    chunk_size = int(os.environ.get("CHUNK_SIZE"))
+
+    connection = psycopg2.connect(host=os.environ.get("PG_HOST"), user=os.environ.get("PG_USER"), password=os.environ.get("PG_PASSWORD"), dbname=os.environ.get("PG_DATABASE"))
+    cursor = connection.cursor()
+    cursor.execute(os.environ.get("PAGES_TO_SCRAPE_QUERY"))
+    careers_page_urls = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    url_chunks = get_url_chunks(careers_page_urls, chunk_size)
+
+    num_processes = len(url_chunks)
+    processes = []
+
+    for i, single_url_chunk in enumerate(url_chunks):
+        p = multiprocessing.Process(target=run_spider, args=(single_url_chunk, i))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
