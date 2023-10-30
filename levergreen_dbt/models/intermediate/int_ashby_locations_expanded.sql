f916a0333b307e31a4c1323a9f1289838ba81a48
Andrew D.Gramigna <agramign@cs.umass.edu>
2023-10-30 09:57:26 -0400

    add locations as comma separated list

---
+++ levergreen_dbt/models/intermediate/int_ashby_locations_expanded.sql
@@ -0,0 +1,23 @@
+with job_locations as (
+    select * from {{ ref('stg_ashby__job_locations') }}
+),
+
+jobs_outline as (
+    select * from {{ ref('stg_ashby__jobs_outline') }}
+),
+
+all_secondary_locations as (
+    select opening_id, string_agg(secondary_location_name, ', ') as secondary_locations from job_locations
+    group by 1
+)
+
+select
+    jobs_outline.id,
+    jobs_outline.opening_id,
+    case 
+        when all_secondary_locations.secondary_locations is null
+            then jobs_outline.location_name
+        else concat(jobs_outline.location_name, ', ', all_secondary_locations.secondary_locations) 
+    end as location
+from jobs_outline
+left join all_secondary_locations on jobs_outline.opening_id = all_secondary_locations.opening_id
