64412fe4164214cbb20b97111e1991eda5da2d66
Andrew Gramigna <adgramigna1@gmail.com>
2024-05-11 00:02:36 -0400

    adjust default job postings to remove not is_prospect filter and increase id

--- levergreen_dbt/models/marts/active_job_postings_default.sql
+++ levergreen_dbt/models/marts/active_job_postings_default.sql
@@ -1,5 +1,4 @@
 select active_job_postings.* from {{ ref('active_job_postings') }} active_job_postings
 inner join {{ source('levergreen', 'job_board_urls') }} job_board_urls 
     on active_job_postings.source = job_board_urls.url
-where 
-(not job_board_urls.is_prospect or job_board_urls.id <= 19)
+where job_board_urls.id <= 22
